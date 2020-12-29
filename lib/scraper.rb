4cd374d85b31a79708b9c877914f905df9daf074
chrislemus <icrislemus@gmail.com>
2020-12-28 20:56:31 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html) 
+heading_text = doc.css('.headline-26OIBN').text
+courses = doc.css('.title-oE5vT4').slice(0,4)
+
+puts "\n----HOMEPAGE HEADING----\n #{heading_text}"
+puts "\n----AVAILABLE COURSES----"
+courses.each {|course| puts course.text}
+# binding.pry
